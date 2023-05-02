d9bd10418b121269fd37dacad906760daabe3f2c
pdh <pdh930105@gmail.com>
2023-05-02 04:22:19 +0000

    update neurosim trackking code

--- utils.py
+++ utils.py
@@ -26,35 +26,80 @@ class View(torch.nn.Module):
 
 def Neural_Sim(self, input, output):
     global model_n
+    global model_name
     print("quantize layer ", self.name)
     input_file_name = './layer_record_' + str(model_n) + '/input' + self.name + '.csv'
     weight_file_name = './layer_record_' + str(model_n) + '/weight' + self.name + '.csv'
     f = open('./layer_record_' + str(model_n) + '/trace_command.sh', 'a')
     f.write(weight_file_name+' '+input_file_name+' ')
+    #format 'input_height, input_width, input_channel, kernel_height, kernel_width, output_channel, pooling, stride, wl_input, wl_weight'
+    
+    
     if 'TBNConv' in self.name or 'TBNLinear' in self.name:
         weight_q = self.weight_quantizer(self.weight)
         weight_q = weight_q.sign() # -1, 1
         inputs_q = self.input_quantizer(input[0]).sign() # -1, 0, 1
         length = 2 # 2 bit (-1 = 10, 0 = 00, 1 = 01)
+        network_config_csv_format = 'input_height, input_width, input_channel, kernel_height, kernel_width, output_channel, pooling, stride, wl_input, wl_weight\n'
+        f = open('./layer_record_'+str(model_name)+'/NetWork_'+str(model_name)+'.csv', 'a')
         write_matrix_weight(weight_q.cpu().detach().numpy(), weight_file_name)
         if len(self.weight.shape) > 2 : # convolution
             k = self.weight.shape[-1] # kw, kh size
             padding = self.padding
             stride = self.stride
+            input_height, input_width, input_channel = input[0].shape[2], input[0].shape[3], input[0].shape[1]
+            output_channel = self.weight.shape[0]
+            pooling = 0
+            wl_input = self.wl_input
+            wl_weight = self.wl_weight
+
+            network_config_csv_format = f'{input_height}, {input_width}, {input_channel}, {k}, {k}, {output_channel}, {pooling}, {stride[0]}, {wl_input}, {wl_weight}\n'
+            f.write(network_config_csv_format)
             write_matrix_activation_conv(stretch_input(inputs_q.cpu().data.numpy(),k,padding,stride),None, length, input_file_name)
+            print("Write finish : ", self.name)
         else:
+            input_height, input_width = 1
+            input_channel = input[0].shape[-1]
+            output_channel = self.weight.shape[0]
+            pooling = 0
+            stride=1
+            wl_input = self.wl_input
+            wl_weight = self.wl_weight
+            network_config_csv_format = f'{input_height}, {input_width}, {input_channel}, {k}, {k}, {output_channel}, {pooling}, {stride[0]}, {wl_input}, {wl_weight}\n'
+            f.write(network_config_csv_format)
             write_matrix_activation_fc(inputs_q.cpu().data.numpy(),None , length, input_file_name)
+            print("Write finish : ", self.name)
     else: # QConv2d , QLinear
         weight_q = wage_quantizer.Q(self.weight, self.wl_weight)
         write_matrix_weight(weight_q.cpu().detach().numpy(), weight_file_name)
+        f = open('./layer_record_'+str(model_name)+'/NetWork_'+str(model_name)+'.csv', 'a')
         if len(self.weight.shape) > 2 : # convolution
             k = self.weight.shape[-1] # kw, kh size
             padding = self.padding
             stride = self.stride
+            input_height, input_width, input_channel = input[0].shape[2], input[0].shape[3], input[0].shape[1]
+            output_channel = self.weight.shape[0]
+            pooling = 0
+            wl_input = self.wl_input
+            wl_weight = self.wl_weight
+
+            network_config_csv_format = f'{input_height}, {input_width}, {input_channel}, {k}, {k}, {output_channel}, {pooling}, {stride[0]}, {wl_input}, {wl_weight}\n'
+            f.write(network_config_csv_format)
             write_matrix_activation_conv(stretch_input(input[0].cpu().data.numpy(),k,padding,stride),None, self.wl_input, input_file_name)
+            print("Write finish : ", self.name)
         else:
+            input_height = input_width = 1
+            input_channel = input[0].shape[-1]
+            output_channel = self.weight.shape[0]
+            k = 1
+            pooling = 0
+            stride=[1]
+            wl_input = self.wl_input
+            wl_weight = self.wl_weight
+            network_config_csv_format = f'{input_height}, {input_width}, {input_channel}, {k}, {k}, {output_channel}, {pooling}, {stride[0]}, {wl_input}, {wl_weight}\n'
+            f.write(network_config_csv_format)
             write_matrix_activation_fc(input[0].cpu().data.numpy(),None , self.wl_input, input_file_name)
-    
+            print("Write finish : ", self.name)
 
 def write_matrix_weight(input_matrix, filename):
     cout = input_matrix.shape[0] # output channel
@@ -141,9 +186,10 @@ def remove_hook_list(hook_handle_list):
     for handle in hook_handle_list:
         handle.remove()
 
-def hardware_evaluation(model,wl_weight,wl_activation,model_name,mode): 
-    global model_n
-    model_n = model_name
+def hardware_evaluation(model,wl_weight,wl_activation,arch_name,mode): 
+    global model_n, model_name
+    model_n = arch_name
+    model_name = arch_name
     
     hook_handle_list = []
     if not os.path.exists('./layer_record_'+str(model_name)):
@@ -151,7 +197,9 @@ def hardware_evaluation(model,wl_weight,wl_activation,model_name,mode):
     if os.path.exists('./layer_record_'+str(model_name)+'/trace_command.sh'):
         os.remove('./layer_record_'+str(model_name)+'/trace_command.sh')
     f = open('./layer_record_'+str(model_name)+'/trace_command.sh', "w")
-    f.write('./NeuroSIM/main ./NeuroSIM/NetWork_'+str(model_name)+'.csv '+str(wl_weight)+' '+str(wl_activation)+' ')
+    if os.path.exists('./layer_record_'+str(model_name)+'/NetWork_'+str(model_name)+'.csv'):
+        os.remove('./layer_record_'+str(model_name)+'/NetWork_'+str(model_name)+'.csv')
+    f.write('./NeuroSIM/main ./NetWork_'+str(model_name)+'.csv '+str(wl_weight)+' '+str(wl_activation)+' ')
     
     for i, layer in enumerate(model.modules()):
         if isinstance(layer, (TBNConv2d, QConv2d)) or isinstance(layer, (QLinear, TBNLinear)):
